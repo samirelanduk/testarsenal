f001eb42c817fea6e84965a14f0b9113bfa6bc85
Sam Ireland <sam.ireland.uk@gmail.com>
2017-12-01 12:43:22 +0000

    First commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name="seleniumx",
+      version="0.1.0",
+      description="Selenium tools",
+      long_description="Tools for making using Selenium easier",
+      url="https://github.com/samirelanduk/seleniumx",
+      author="Sam Ireland",
+      author_email="mail@samireland.com",
+      license="MIT",
+      classifiers=["Development Status :: 4 - Beta",
+                   "Intended Audience :: Other Audience",
+                   "License :: OSI Approved :: MIT License",
+                   "Topic :: Communications",
+                   "Programming Language :: Python :: 3",
+                   "Programming Language :: Python :: 3.5",
+                   "Programming Language :: Python :: 3.6"],
+      install_requires=[])
