b51a8afa606fe9b0e4c6c892545acf7a7d1b68c4
Sam Ireland <sam.ireland.uk@gmail.com>
2018-05-26 11:48:49 +0100

    Update version in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="testarsenal",
-      version="0.2.0",
+      version="0.3.0",
       description="Testing tools",
       long_description="Tools for making using selenium and django testing easier",
       url="https://github.com/samirelanduk/testarsenal",
